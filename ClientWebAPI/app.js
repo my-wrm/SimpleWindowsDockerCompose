73ab73bb3bb9f87e6f518ea74be11a56381bfb41
Thada Wangthammang <thada.wth@gmail.com>
2021-10-25 15:54:39 +0700

    add nodejs project

--- ClientWebAPI/app.js
+++ ClientWebAPI/app.js
@@ -1,20 +1,14 @@
 const axios = require('axios');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.get('/', async (req, res) => {
-  res.send(`[Client] Hello World! read from server: ${process.env.SERVER_HOST}`)
+  res.send(`[ClientWebAPI] Hello World! read from server: ${process.env.SERVER_HOST}`)
 });
 
-app.get('/call-api', async (req, res) => {
-  try{
-    const result = await axios.get(`http://${process.env.SERVER_HOST}/api`);
-    res.send(JSON.stringify(result.data));
-  } catch (e){
-    res.send("Something error");
-    console.error(e);
-  }
+app.get('/api', (req, res) => {
+  res.json({ value: "Here is data from ClientWebAPI" });
 });
 
 app.get('/WeatherForecast', async (req, res) => {
@@ -25,8 +19,10 @@ app.get('/WeatherForecast', async (req, res) => {
     res.send("Something error");
     console.error(e);
   }
+  
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
