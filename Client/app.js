3b595e603f2f1e93c94842b9ab9fafb4d86876d2
Thada Wangthammang <thada.wth@gmail.com>
2021-10-25 15:13:04 +0700

    steup nodejs image

--- Client/app.js
+++ Client/app.js
@@ -1,9 +1,21 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  res.send(`Hello World! read from server: ${process.env.SERVER_HOST}`)
+});
+
+app.get('/WeatherForecast', async (req, res) => {
+  try{
+    const result = await axios.get(`${process.env.SERVER_HOST}/WeatherForecast`);
+    res.send(JSON.stringify(result.data));
+  } catch (e){
+    res.send("Something error");
+    console.error(e);
+  }
+  
 });
 
 app.listen(port, () => {
